327a67c266a7093df84971748af2d910ce4bb305
ZRIBA Yassine <ia.ssine3325@gmail.com>
2026-03-26 15:56:20 +0100

    feat: add CategoryRepository with Spring Data JPA

---
+++ src/main/java/fr/fms/springbasics/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package fr.fms.springbasics.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.fms.springbasics.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+	
+}
