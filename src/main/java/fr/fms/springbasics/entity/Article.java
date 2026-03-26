d8d7a3fdd9ce09730bef525e872ea3860ab8b2a6
ZRIBA Yassine <ia.ssine3325@gmail.com>
2026-03-26 14:05:21 +0100

    feat: add first JPA entity Article

---
+++ src/main/java/fr/fms/springbasics/entity/Article.java
@@ -0,0 +1,69 @@
+package fr.fms.springbasics.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Article implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String brand;
+    private String description;
+    private Double price;
+    
+    public Article() {
+    	// Constructeur vide obligatoire pour JPA
+    }
+    
+    public Article(Long id, String brand, String description, Double price) {
+    	this.id = id;
+    	this.brand = brand;
+    	this.description = description;
+    	this.price = price;
+    }
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+    
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", brand=" + brand + ", description=" + description + ", price=" + price + "]";
+	}
+}
